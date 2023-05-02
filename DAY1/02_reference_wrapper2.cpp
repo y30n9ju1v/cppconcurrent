7b1484ce02a3b8b15ef25245c5dfc92440fe1868
ecourse <smkang@codenuri.co.kr>
2023-05-02 10:18:09 +0900

    Add files via upload

--- DAY1/02_reference_wrapper2.cpp
+++ DAY1/02_reference_wrapper2.cpp
@@ -12,6 +12,18 @@ public:
 	// raw reference 로 암시적 변환 허용
 	operator T& () { return *ptr; }
 };
+
+// reference_wrapper 개념
+// => T& 처럼 참조로 동작하게 만든 간단한 클래스
+// => raw reference 와의 차이점 대입 연산시
+//    raw reference(T&) : 가리키던 값이 이동
+//    reference_wrapper : 참조객체 자체가 이동
+
+// 원리
+// => 내부적으로 대상체의 주소를 보관
+// => raw reference 로 변환도 가능
+
+
 int main()
 {
 	int v1 = 10, v2 = 20;
