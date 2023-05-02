6d38d442782b31833e81fc09e42709f7e66a0781
ecourse <smkang@codenuri.co.kr>
2023-05-02 16:23:30 +0900

    Add files via upload

--- DAY1/04_thread9-3.cpp
+++ DAY1/04_thread9-3.cpp
@@ -51,8 +51,36 @@ RT parallel_sum(IT first, IT last, RT init)
     const std::size_t block_size = cnt_element / cnt_thread;
                     // => 한개의 스레드가 처리할 요소의 갯수
 
-    return 0;
+    // 실제 생성할 스레드 : 스레드 갯수-1 (주스레드도 있으므로)
+    std::vector<std::thread> thread_vec(cnt_thread - 1); 
+    std::vector<RT> result_vec(cnt_thread);
+    
+    IT start = first;
+
+    for (int i = 0; i < cnt_thread - 1; ++i)
+    {
+        IT end = std::next(start, block_size);
+
+        thread_vec[i] = std::thread(sum<std::vector<int>::iterator, int>,
+                             start, end, std::ref(result_vec[i]));
+
+        start = end;
+    }
+    // 마지막 구간(start, last)은 주스레드가 수행합니다.
+    sum(start, last, result_vec[cnt_thread - 1]);
+
+    // 모든 스레드가 종료될때를 대기
+    for (auto& t : thread_vec)
+        t.join();
+
+    // 이제 결과를 담은 vector의 합을 반환하면 됩니다.
+    RT ret = std::accumulate(result_vec.begin(),
+                            result_vec.end(), 0);
+
+    return ret;
 }
+
+
 
 
 
