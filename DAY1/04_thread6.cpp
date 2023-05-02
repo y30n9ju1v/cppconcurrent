32f05dfd11f20ced1b69d01aebde937fb3dd2a30
ecourse <smkang@codenuri.co.kr>
2023-05-02 13:54:11 +0900

    Add files via upload

--- DAY1/04_thread6.cpp
+++ DAY1/04_thread6.cpp
@@ -25,7 +25,17 @@ int main()
     std::thread t3(&f1);
     std::thread t4(&f2);
 
+    // 스레드가 실행중이건 종료 되었건 t3, t4 는 모두 join가능
     bool b1 = t3.joinable(); // f1이 실행중이건
     bool b2 = t4.joinable(); // f2가 종료되었건
                              // 모두 true
+
+    // 스레드를 만들지 않거나
+    // 이미 join(또는 detach) 한 경우만 false 입니다.
+    std::thread t5;
+    std::thread t6(&f2);
+    t6.join(); // t6.detach();
+
+    bool b1 = t5.joinable();  // 스레드 생성 안됨
+    bool b2 = t6.joinable();  // 이미 join 했음
 }
