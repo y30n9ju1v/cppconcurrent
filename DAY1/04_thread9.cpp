e4e643ac0639e1e878035d7e03f4edad5eeb777f
ecourse <smkang@codenuri.co.kr>
2023-05-02 15:30:34 +0900

    Add files via upload

--- DAY1/04_thread9.cpp
+++ DAY1/04_thread9.cpp
@@ -32,9 +32,16 @@ int main()
     int s = 0;
 //  sum(v.begin(), v.end(), s); // 주스레드가 직접 호출
 
-    std::thread t(sum, v.begin(), v.end(), std::ref(s));
+//  std::thread t(sum, v.begin(), v.end(), std::ref(s));
                 // 왜 에러일까요 ?
+                // sum 이 템플릿인데. 
+                // => 직접 호출하면 인자 타입 추론가능
+                // => 하지만 이름만 전달하므로 타입 추론안됨
 
+    // 아래 처럼 타입 전달해야 합니다.
+    std::thread t(sum<std::vector<int>::iterator, int>, 
+                v.begin(), v.end(), std::ref(s));
+    t.join();
 
     std::cout << s << std::endl;
 }
