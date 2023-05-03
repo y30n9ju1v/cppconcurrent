bbe7a1767c840673361293f43ea7ef32af1e41cb
ecourse <smkang@codenuri.co.kr>
2023-05-03 10:44:20 +0900

    Add files via upload

--- DAY2/02_packaged_task2.cpp
+++ DAY2/02_packaged_task2.cpp
@@ -20,10 +20,24 @@ int main()
 	std::future<int> ft = task.get_future();
 
 	// 3. 주스레드가 실행해도 됩니다.
-	task(1, 2);  // () 연산자가 재정의 되어 있으므로
+//	task(1, 2);  // () 연산자가 재정의 되어 있으므로
 				 // 이렇게 호출가능
-	int ret1 = ft.get();
+				 // add(1, 2)
+//	int ret1 = ft.get();
+
+	// 4. 새로운 스레드를 만들때 1번째 인자로 task 전달하면 됩니다.
+	// std::ref(task)  : task 를 계속 사용하고 싶을때
+	// std::move(task) : task 객체가 더이상 필요 없다면
+	//std::thread t(std::ref(task), 1, 2); 
+	std::thread t(std::move(task), 1, 2);
+	
+	int ret2 = ft.get();
+	t.join();
 }
 
-
+// promise & future : 함수인자로 promise 를 받아야 합니다
+// 
+// packaged_task    : 스레드 함수가 아닌 일반 함수 모양으로
+//						만들어진 함수를 스레드로 수행해서 결과 얻기
+//						"보관" 했다가 "원할때 실행"
 
