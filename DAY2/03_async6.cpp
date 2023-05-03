6c0994ad50a2873b08d898a965f6d806a7849636
ecourse <smkang@codenuri.co.kr>
2023-05-03 13:20:52 +0900

    Add files via upload

--- DAY2/03_async6.cpp
+++ DAY2/03_async6.cpp
@@ -14,7 +14,10 @@ void foo()
 int main()
 {
 	// 반환 값이 없으면 아래 처럼 하면 됩니다.
-	std::future<void> ft = std::async(foo);
+	// 그럼 옵션이 시스템 상황에 맞지 않는 경우에는, 예외가 발생 할까요?
+	// => 아래 처럼 한후. 멀티스레드가 지원되지 않은 시스템에서 실행하면
+	//    "std::system_error" 타입의 예외 발생
+	std::future<void> ft = std::async(std::launch::async, foo);
 	try
 	{
 		ft.get();
@@ -27,3 +30,6 @@ int main()
 	// async 사용시에 join이나 detach은 따로 필요하지 않나요 ?
 	// => 필요 없습니다.
 }
+// "return 값"의 종류에 따른 제약은 없을까요?
+// std::tuple 이라던지, task 자체도 return 할 수 있나요?
+// => 특별한 제약없습니다. task 도 가능.
