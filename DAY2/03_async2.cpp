e43facadff04c6ceb54a78517f9807ac17fe8686
ecourse <smkang@codenuri.co.kr>
2023-05-03 11:24:18 +0900

    Add files via upload

--- DAY2/03_async2.cpp
+++ DAY2/03_async2.cpp
@@ -19,10 +19,14 @@ int main()
     //                         스레드 만들지 말고, 결과가 필요할때
     //                         주스레드가 호출.!
 //  std::future<int> ft = std::async(std::launch::async,    add, 10, 20);
-    std::future<int> ft = std::async(std::launch::deferred, add, 10, 20);
+//  std::future<int> ft = std::async(std::launch::deferred, add, 10, 20);
 
 
-//    std::future<int> ft = std::async( add, 10, 20);
+    // std::launch::async || std::launch::deferred
+    // ==> 스레드를 만들수 있으면 스레드로, 
+    // ==> 스레드가 지원되지 않은 OS 라면 지연된 실행으로
+    // ==> 옵션 생략시, 디폴트값
+    std::future<int> ft = std::async( add, 10, 20);
 
 
     std::cout << "continue main : " << std::this_thread::get_id() << std::endl;
