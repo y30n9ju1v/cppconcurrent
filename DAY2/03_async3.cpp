4a5df21b20b673cac0db830ac25f89df94be6d78
ecourse <smkang@codenuri.co.kr>
2023-05-03 11:32:03 +0900

    Add files via upload

--- DAY2/03_async3.cpp
+++ DAY2/03_async3.cpp
@@ -14,7 +14,6 @@ int add(int a, int b)
     return a + b;
 }
 
-
 int main()
 {
     std::future<int> ft = std::async(add, 10, 20);
@@ -24,7 +23,9 @@ int main()
     // async 가 반환한 future 를 get() 하지 않으면  ?
 //  int ret = ft.get(); // 이순간 결과가 나올때 까지 대기.
     
-}
+} // <= 이순간 ft 가 파괴 됩니다.
+  // <= ft 의 소멸자가 호출되는데.. ft.get() 을 한번도 하지 않은경우
+  // <= 소멸자가 ft.get() 을 실행해서 결과를 대기 하기 됩니다.
 
 
 
