738da1680835fc977afd416115ab195f5fc3d65e
ecourse <smkang@codenuri.co.kr>
2023-05-04 10:21:54 +0900

    Add files via upload

--- DAY3/02_producer_consume3.cpp
+++ DAY3/02_producer_consume3.cpp
@@ -11,6 +11,11 @@ std::mutex m;
 
 std::condition_variable cv;
 
+// 아래 코드는 cv 를 사용해서 Q가 비어있을때 
+// 소비자를 대기하게 하는 코드 입니다.
+
+// 마지막 문제는 생산자가 너무 빨라서 Q가 가득 찰때 입니다.
+// => 과제로 해보세요..(Q.size() == 10000 등의 조건으로 해보세요)
 
 void producer()
 {
@@ -40,9 +45,17 @@ void consume()
 			// 2. 무조건 Q를 사용하지 말고..,.
 			//    조건을 만족할때만 사용
 			//    조건을 만족하지 않으면 unlock 하고 대기
-			cv.wait(ul, []() { !Q.empty(); });
+
+//			cv.wait(ul, !Q.empty()); // error
+							// 2번째 인자는 bool 이 아닌
+							// 함수 이어야 합니다.
+							// 아래 처럼 람다표현식등을 사용하세요
+
+			cv.wait(ul, []() { return !Q.empty(); });
 					// => 조건을 조사 할때 Q의 멤버함수를 사용
 					// => 별도의 전역변수가 없어도 됩니다.
+
+
 
 
 			int element = Q.front();
