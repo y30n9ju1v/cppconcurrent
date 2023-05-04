276301e37c143f6c371e0d4324dd41b6280afa88
ecourse <smkang@codenuri.co.kr>
2023-05-04 11:42:22 +0900

    Add files via upload

--- DAY3/03_thread_pool4-1.cpp
+++ DAY3/03_thread_pool4-1.cpp
@@ -11,6 +11,9 @@ int main()
 	// std::function 은 std::bind 를 사용하면 인자를 고정해서
 	// 보관가능
 	std::function<void()> f1;
-	f1 = &foo;
-	f1();
+	//f1 = &foo;	// error. foo 는 인자가 2개 인데
+				// f1은 인자가 없는 함수 만 보관
+
+	f1 = std::bind(&foo, 1, 2); // foo 와 인자 2개를 같이 보관
+	f1(); // foo(1,2);
 }
