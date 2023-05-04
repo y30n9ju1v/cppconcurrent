d66ccd0c2e03115ab0f9413606ca4d1d2f4f17d6
ecourse <smkang@codenuri.co.kr>
2023-05-04 11:16:05 +0900

    Add files via upload

--- DAY3/03_thread_pool2.cpp
+++ DAY3/03_thread_pool2.cpp
@@ -8,25 +8,33 @@ using namespace std::literals;
 
 void foo();
 
+//-------------------------------------------
+std::vector<std::thread> v;
+
 using TASK = void(*)();
 
 std::queue<TASK> task_q;
 std::mutex m;
 std::condition_variable cv;
 
-bool stop = false;
-std::vector<std::thread> v;
+bool stop = false; // 스레드 풀 종료 할때 사용
 
+//-----------------------------
+// 스레드 풀의 모든 스레드는 아래 함수를 실행하게 됩니다.
+void pool_thread_main()
+{
+}
 
-
-
-
-
-
+void init_pool(int cnt)
+{
+	// 요청한 갯수만큼의 스레드를 만들어서 pool_thread_main 실행
+	for (int i = 0; i < cnt; i++)
+		v.emplace_back(pool_thread_main);
+}
 
 int main()
 {
-
+	init_pool(10);
 }
 
 
