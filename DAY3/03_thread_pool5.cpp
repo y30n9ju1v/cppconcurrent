53777b9e7b7779c9043980c261c5fd7da66d4e93
ecourse <smkang@codenuri.co.kr>
2023-05-04 13:11:11 +0900

    Add files via upload

--- DAY3/03_thread_pool5.cpp
+++ DAY3/03_thread_pool5.cpp
@@ -25,12 +25,22 @@ int main()
 
 	// packaged_task 를 사용하면 아직 실행되지 않은 함수의 
 	// 결과를 얻을수 있습니다.
-	std::packaged_task<int(int, int)> task(add);
+
+	// 1. 아래 처럼 만들면 task 사용시 인자 필요
+//	std::packaged_task<int(int, int)> task(add);
+//	task(1, 2);
+
+	// 2. 아래 처럼 만들면 task 사용시 인자 필요 없습니다.
+	std::packaged_task<int()> task(
+					std::bind(add, 1, 2));
 
 	// add 가 실행되지 않았지만 미래의 결과를 담은 객체는
 	// 꺼낼수 있습니다.
 	std::future<int> ft = task.get_future(); 
 
-	int ret2 = ft.get();
+	task(); // add(1,2)
+
+
+	int ret2 = ft.get(); // 결과 대기
 
 }
